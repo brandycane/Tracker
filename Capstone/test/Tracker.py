30372bc507f0455f3ce40cd1a26326c5057bce9c
Brandy Thompson <158996678+brandycane@users.noreply.github.com>
2026-04-14 12:53:15 -0700

    Add files via upload

---
+++ Capstone/test/Tracker.py
@@ -0,0 +1,13 @@
+print("Go the Distance")
+print("Welcome to your Workout")
+print("Choose your Workout")
+
+for int in interval_timer:
+
+
+#display time or distance radio buttons
+
+def move(a,b):
+    #use for tracker initialization and tracking
+def interval():
+    #length of workout section, no limit
